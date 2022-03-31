7ad56356c560436a6f1c7b63253cb4e2c6d0c71c
Fabrizio Marconi <fabrizio.marconi@jef.it>
2022-03-31 18:02:47 +0200

    last commit!

--- express/v3.js
+++ express/v3.js
@@ -10,7 +10,7 @@ v3Router.get('/', (req, res) => {
 });
 
 const controllaSeIlFileEsiste = (req, res, next) => {
-  const nomeFile = req.params.nomeFile;
+  const nomeFile = 'files/' + req.params.nomeFile;
   console.log('file richiesto: ' + nomeFile);
   if (!fs.existsSync(nomeFile)) {
     return res.status(404).json({
@@ -20,11 +20,30 @@ const controllaSeIlFileEsiste = (req, res, next) => {
   next();
 }
 
+v3Router.get('/contatori', (req, res) => {
+  console.log('siamo nella rotta contatore lista v3');
+
+  try {
+    const listFiles = fs.readdirSync('files/');
+
+    const listaFiltrata = listFiles.filter(
+      nomeFile => nomeFile !== '.gitkeep'
+    )
+    return res.json(listaFiltrata);
+  } catch (e) {
+    console.log(e);
+
+    return res.status(500).json({
+      errore: e.message
+    })
+  }
+});
+
 v3Router.get('/contatori/:nomeFile', controllaSeIlFileEsiste, (req, res) => {
   console.log('siamo nella rotta contatore v3');
 
   try {
-    const nomeFile = req.params.nomeFile;
+    const nomeFile = 'files/' + req.params.nomeFile;
     const contenuto = fs.readFileSync(nomeFile);
     console.log("contenuto file =" + contenuto);
     const nuovocontenuto = parseInt(contenuto);
@@ -44,7 +63,7 @@ v3Router.post('/contatori', (req, res) => {
   console.log('siamo nella rotta di creazione del contatore v3');
 
   try {
-    const nomeFile = req.body.nomeFile;
+    const nomeFile = 'files/' + req.body.nomeFile;
     console.log('file richiesto: ' + nomeFile);
 
     if (!fs.existsSync(nomeFile)) {
@@ -72,7 +91,7 @@ v3Router.delete('/contatori/:nomeFile', controllaSeIlFileEsiste, (req, res) => {
   console.log('siamo nella rotta contatore v3 delete');
 
   try {
-    const nomeFile = req.params.nomeFile;
+    const nomeFile = 'files/' + req.params.nomeFile;
     fs.unlinkSync(nomeFile);
     console.log("abbiamo eliminato il file");
     return res.status(204).send()
@@ -87,7 +106,7 @@ v3Router.delete('/contatori/:nomeFile', controllaSeIlFileEsiste, (req, res) => {
 
 v3Router.put('/contatori/:nomeFile', controllaSeIlFileEsiste, (req, res) => {
   try {
-    const nomeFile = req.params.nomeFile;
+    const nomeFile = 'files/' + req.params.nomeFile;
     const nuovoValore = req.body.nuovoValore;
     fs.writeFileSync(nomeFile, nuovoValore.toString());
     return res.json({
@@ -105,7 +124,7 @@ v3Router.put('/contatori/:nomeFile', controllaSeIlFileEsiste, (req, res) => {
 
 v3Router.patch('/contatori/:nomeFile', controllaSeIlFileEsiste, (req, res) => {
   try {
-    const nomeFile = req.params.nomeFile;
+    const nomeFile = 'files/' + req.params.nomeFile;
     const incremento = req.body.incremento;
 
     let contenuto = fs.readFileSync(nomeFile);
