4f400fbcae8eda394dd8b2f5e032fd157340a9a8
lorenzadriver <lorenza.pilota@itssmartacademy.it>
2022-04-08 18:41:50 +0200

    progetto con rotte persona (dao)

--- studenti/lorenza/progetto_venerdì/index.js
+++ studenti/lorenza/progetto_venerdì/index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
+const {checkPersonaExist, checkAndGetPersonaById} = require('./mw/mw.persona.js')
+const {getPersonaById, personaExistById, listaPersone, createPersona,modificaPersona, aggiornaCampiPersona, cancellaPerId} = require('./db/dao/persona.dao.js')
+const mysql2 = require ('mysql2/promise')
 const express = require('express');
 const { json, urlencoded } = require('body-parser');
-const { routerPersona } = require('./rotte/persona');
+//const { routerPersona } = require('./rotte/persona');
 const app = express()
 
 app.use(express.static('files'));
@@ -9,34 +12,63 @@ app.use(json());
 app.use(urlencoded({ extended: true }));
 
 
-middleware1= (req, res, next)=>{
-console.log("siamo nel mw1");
-next();
-}
-
-app.get ('/persona/',middleware1, (req, res)=>{
+app.get ('/persona/', async (req, res)=>{
     console.log("siamo nella get da cui otterremo una lista persone");
-    return res.json({
-        message : "va tutto bene, dopo la dao invierò la lista"
-    }).send();
+    const persone = await listaPersone()
+    return res.json(persone).send();
 })
 
-app.get ('/persona/:id_persona',middleware1, (req, res)=>{
+app.get ('/persona/:id_persona',checkAndGetPersonaById, async (req, res)=>{
     console.log("siamo nella get della persona attraverso l'ID");
-    const id_persona= req.params.id_persona;
-    return res.json({
-        message : `va tutto bene, dopo la dao invierò i dati della persona con ID ${id_persona} `
-    }).send();
+    return res.json(req.persona).send();
 })
 
-app.post ('/persona/', middleware1, (req, res)=>{
+app.post ('/persona/',  async (req, res)=>{
     console.log("siamo nella post: creo una persona");
     const { nome, cognome, codice_fiscale, data_nascita } = req.body;
+    const id_persona = await createPersona(nome, cognome, codice_fiscale, data_nascita )
     return res.json({
-        message : "va tutto bene, dopo la dao inserirò la persona"
-    }).send();
+        "id_persona_inserita ":id_persona}).send(); 
+    })
+
+app.put ('/persona/:id_persona', checkPersonaExist, async (req, res)=>{
+    console.log("siamo nella put: modifico un record persona");
+    const { nome, cognome, codice_fiscale, data_nascita } = req.body;
+    const id_persona = req.params.id_persona;
+    const statoRichiesta = await modificaPersona(id_persona, nome, cognome, codice_fiscale, data_nascita )
+    if(!statoRichiesta){
+        return res.status(500).json({
+            message: "c'è stato un problema"
+        }).send();
+    }
+    res.status(204).send();; 
+    })
+
+app.patch ('/persona/:id_persona', checkPersonaExist, async(req, res)=>{
+    let { nome, cognome, codice_fiscale, data_nascita } = req.body;
+    const id_persona = req.params.id_persona;
+    const ok = await aggiornaCampiPersona(id_persona, nome, cognome, codice_fiscale, data_nascita);
+    if (ok) {
+      return res.status(204).send()
+    } else {
+      return res.status(500).json({
+        message: "c'è stato un errore"
+      });
+    }
 })
 
+app.delete('/persona/:id_persona', checkPersonaExist, async (req, res) => { 
+    const id_persona = req.params.id_persona;
+    const cancellato = await cancellaPerId(id_persona);
+    if(cancellato){
+    return res.status(200).json({
+      message: 'il record è stato eliminato'
+    }).send()
+    }else{
+       return res.status(500).send();
+    }
+     
+  })
 
 //app.use('/persona', routerPersona);
 
