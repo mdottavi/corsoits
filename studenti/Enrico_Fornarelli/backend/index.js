bd3d0fdc2950b5e38adcbacf4a2dd1284f72c54e
BananaJoe <ousemanessibi@gmail.com>
2022-04-11 16:56:25 +0200

    piccola commit

--- studenti/Enrico_Fornarelli/backend/index.js
+++ studenti/Enrico_Fornarelli/backend/index.js
@@ -1,3 +1,3 @@
 const express=require('express')
 require('dotenv').config();
-console.log(process.env.MYSQL_USER + ' ' + process.env.MYSQL_PASSWORD)
+console.log(process.env.MYSQL_USER + ' ' + process.env.MYSQL_PASSWORD);
