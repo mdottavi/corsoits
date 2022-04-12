dc2e1cad924e2274754f29721f916e3cbae596d8
Mfernanda7 <fernandafabriceno@gmail.com>
2022-04-12 16:06:51 +0200

    Aggiornamento

--- studenti/Fernanda/backend/index.js
+++ studenti/Fernanda/backend/index.js
@@ -1,20 +1,21 @@
 
-const express=require('express');
+const express = require('express');
 
 require('dotenv').config();
-const{json, urlencoded}=require('body-paarser');
-const fileUpload=require('express-fileupload');
-const ConnectRouter=require('./routes/main-router');
+const { json, urlencoded } = require('body-parser');
+const fileUpload = require('express-fileupload');
+const ConnectRouter = require ('./routes/main-router');
 
-console.log(process.env.MYSQL_USER+""+process.env.MYSQL_PASSWORD)
+console.log(process.env.MYSQL_USER + " " + process.env.MYSQL_PASSWORD);
 
-const app=express()
+const app = express()
 
 app.use(express.static('files'));
 app.use(json());
-app.use(urlencoded())
+app.use(urlencoded({ extended: true }));
 app.use(fileUpload());
 
+app.set('view engine', 'ejs');
 ConnectRouter(app);
 
-app.listen();
+app.listen(9000);
