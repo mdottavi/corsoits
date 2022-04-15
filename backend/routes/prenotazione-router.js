72c778b5a0f753715ab9a6912c32e979945ad390
maurizio <maurizio.d@gate-away.com>
2022-04-15 18:05:27 +0200

    PUT prenotazione

--- backend/routes/prenotazione-router.js
+++ backend/routes/prenotazione-router.js
@@ -8,8 +8,10 @@ const PrenotazioneController = require ('../controllers/PrenotazioneController')
  */
  routerPrenotazione.get('/', PrenotazioneController.lista);
  routerPrenotazione.post('/', PrenotazioneController.crea);
- routerPrenotazione.get('/crea', PrenotazioneController.bakeCreationData );
+ routerPrenotazione.get('/crea', PrenotazioneController.bakeCreationUpdateData );
  routerPrenotazione.delete('/:id', PrenotazioneController.checkId, PrenotazioneController.elimina);
  routerPrenotazione.get('/:id', PrenotazioneController.checkId, PrenotazioneController.get);
 
+ routerPrenotazione.put('/:id', PrenotazioneController.checkId, PrenotazioneController.edit);
+ routerPrenotazione.get('/:id/mod', PrenotazioneController.checkId, PrenotazioneController.bakeCreationUpdateData);
 module.exports = routerPrenotazione;
