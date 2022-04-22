d96adb8c320677ea1e9a246f3f9c342c32e5b3b1
Fabrizio Marconi <fabrizio.marconi@jef.it>
2022-04-22 11:17:51 +0200

    campi unsigned e autoincrement

--- piattaforma_vaccini_v2.sql
+++ piattaforma_vaccini_v2.sql
@@ -1,94 +1,32 @@
--- MySQL Workbench Forward Engineering
+-- MySQL Workbench Synchronization
+-- Generated: 2022-04-22 11:16
+-- Model: New Model
+-- Version: 1.0
+-- Project: Name of the project
+-- Author: tdlem
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
--- Schema piattaforma_vaccini_v2
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema piattaforma_vaccini_v2
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `piattaforma_vaccini_v2` DEFAULT CHARACTER SET utf8 ;
-USE `piattaforma_vaccini_v2` ;
 
--- -----------------------------------------------------
--- Table `piattaforma_vaccini_v2`.`sede`
--- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `piattaforma_vaccini_v2`.`sede` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NOT NULL,
   `citta` VARCHAR(255) NOT NULL,
-  `indirizzo` VARCHAR(255) NULL,
+  `indirizzo` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-
--- -----------------------------------------------------
--- Table `piattaforma_vaccini_v2`.`opertore`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `piattaforma_vaccini_v2`.`opertore` (
-  `id` INT NOT NULL,
-  `ruolo` VARCHAR(90) NOT NULL,
-  `nome` VARCHAR(90) NOT NULL,
-  `cognome` VARCHAR(90) NOT NULL,
-  `username` VARCHAR(90) NOT NULL,
-  `password` VARCHAR(90) NOT NULL,
-  `sede_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_opertore_sede1_idx` (`sede_id` ASC),
-  CONSTRAINT `fk_opertore_sede1`
-    FOREIGN KEY (`sede_id`)
-    REFERENCES `piattaforma_vaccini_v2`.`sede` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `piattaforma_vaccini_v2`.`somministrazione`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `piattaforma_vaccini_v2`.`somministrazione` (
-  `id` INT NOT NULL,
-  `vaccino` VARCHAR(45) NOT NULL,
-  `dose` VARCHAR(45) NOT NULL,
-  `data_somministrazione` DATE NOT NULL,
-  `note` TEXT NOT NULL,
-  `opertore_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_somministrazione_opertore1_idx` (`opertore_id` ASC),
-  CONSTRAINT `fk_somministrazione_opertore1`
-    FOREIGN KEY (`opertore_id`)
-    REFERENCES `piattaforma_vaccini_v2`.`opertore` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `piattaforma_vaccini_v2`.`persona`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `piattaforma_vaccini_v2`.`persona` (
-  `id` INT NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
-  `cognome` VARCHAR(45) NOT NULL,
-  `codice_fiscale` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `piattaforma_vaccini_v2`.`prenotazione`
--- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `piattaforma_vaccini_v2`.`prenotazione` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `data` DATETIME NOT NULL,
-  `sede_id` INT UNSIGNED NOT NULL,
-  `somministrazione_id` INT NULL,
-  `note` VARCHAR(45) NULL,
-  `persona_id` INT NOT NULL,
+  `sede_id` INT(10) UNSIGNED NOT NULL,
+  `somministrazione_id` INT(10) UNSIGNED NULL DEFAULT NULL,
+  `note` VARCHAR(45) NULL DEFAULT NULL,
+  `persona_id` INT(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_prenotazione_sede_idx` (`sede_id` ASC),
   INDEX `fk_prenotazione_somministrazione1_idx` (`somministrazione_id` ASC),
@@ -108,16 +46,64 @@ CREATE TABLE IF NOT EXISTS `piattaforma_vaccini_v2`.`prenotazione` (
     REFERENCES `piattaforma_vaccini_v2`.`persona` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
+CREATE TABLE IF NOT EXISTS `piattaforma_vaccini_v2`.`persona` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `cognome` VARCHAR(45) NOT NULL,
+  `codice_fiscale` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
--- -----------------------------------------------------
--- Table `piattaforma_vaccini_v2`.`token`
--- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `piattaforma_vaccini_v2`.`somministrazione` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `vaccino` VARCHAR(45) NOT NULL,
+  `dose` VARCHAR(45) NOT NULL,
+  `data_somministrazione` DATE NOT NULL,
+  `note` TEXT NOT NULL,
+  `opertore_id` INT(10) UNSIGNED NOT NULL,
+  `persona_id` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_somministrazione_opertore1_idx` (`opertore_id` ASC),
+  INDEX `fk_somministrazione_persona1_idx` (`persona_id` ASC),
+  CONSTRAINT `fk_somministrazione_opertore1`
+    FOREIGN KEY (`opertore_id`)
+    REFERENCES `piattaforma_vaccini_v2`.`opertore` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_somministrazione_persona1`
+    FOREIGN KEY (`persona_id`)
+    REFERENCES `piattaforma_vaccini_v2`.`persona` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `piattaforma_vaccini_v2`.`opertore` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ruolo` VARCHAR(90) NOT NULL,
+  `nome` VARCHAR(90) NOT NULL,
+  `cognome` VARCHAR(90) NOT NULL,
+  `username` VARCHAR(90) NOT NULL,
+  `password` VARCHAR(90) NOT NULL,
+  `sede_id` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_opertore_sede1_idx` (`sede_id` ASC),
+  CONSTRAINT `fk_opertore_sede1`
+    FOREIGN KEY (`sede_id`)
+    REFERENCES `piattaforma_vaccini_v2`.`sede` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
 CREATE TABLE IF NOT EXISTS `piattaforma_vaccini_v2`.`token` (
   `token` VARCHAR(45) NOT NULL,
-  `opertore_id` INT NOT NULL,
-  `exp` BIGINT NOT NULL,
+  `opertore_id` INT(10) UNSIGNED NOT NULL,
+  `exp` BIGINT(19) UNSIGNED NOT NULL,
   `remind_me` TINYINT(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`token`),
   INDEX `fk_token_opertore1_idx` (`opertore_id` ASC),
@@ -126,7 +112,8 @@ CREATE TABLE IF NOT EXISTS `piattaforma_vaccini_v2`.`token` (
     REFERENCES `piattaforma_vaccini_v2`.`opertore` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
